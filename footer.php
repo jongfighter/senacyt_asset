d69f26d1eb4b9215b657a1219406b7cca455111f
jongfighter <qnfeh1111@korea.ac.kr>
2016-08-16 15:14:29 -0500

    footer change

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<footer class="page-footer2 teal">
+<footer class="page-footer teal">
     <div class="footer-copyright">
       <div class="container">
       Made by <a class="brown-text text-lighten-3" href="https://kiv.nia.or.kr/">2016 World Friends ICT Volunteers from KOREA Team Vamos IT </a>
@@ -11,42 +11,4 @@
      </div>
     </div> 
 </footer>
-
-	<script>
-	$(document)
-	.ready(function(){
-        // Add smooth scrolling to all links in navbar + footer link
-        $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
-            // Make sure this.hash has a value before overriding default behavior
-            if (this.hash !== "") {
-                // Prevent default anchor click behavior
-                event.preventDefault();
-
-                // Store hash
-                var hash = this.hash;
-
-                // Using jQuery's animate() method to add smooth page scroll
-                // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-                $('html, body').animate({
-                    scrollTop: $(hash).offset().top
-                }, 900, function(){
-
-                    // Add hash (#) to URL when done scrolling (default click behavior)
-                    window.location.hash = hash;
-                });
-            } // End if
-        });
-
-        $(window).scroll(function() {
-            $(".slideanim").each(function(){
-                var pos = $(this).offset().top;
-
-                var winTop = $(window).scrollTop();
-                if (pos < winTop + 600) {
-                    $(this).addClass("slide");
-                }
-            });
-        });
-    })
-    $('selector').fixedHeaderTable({ footer: true, cloneHeadToFoot: true, fixedColumn: false });
-	</script>
+</div>
