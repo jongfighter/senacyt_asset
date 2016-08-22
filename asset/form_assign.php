f67d0773101b14eeba73e52789f65f56a942b19d
Sara KIM <sara940569@gmail.com>
2016-08-23 04:44:04 +0900

    change datepicker

--- asset/form_assign.php
+++ asset/form_assign.php
@@ -117,7 +117,7 @@ session_start();
   
     $('.datepicker').pickadate({
 		selectMonths: true, // Creates a dropdown to control month
-		selectYears: 15 // Creates a dropdown of 15 years to control year
+		selectYears: 24 // Creates a dropdown of 15 years to control year
 	});
   
 	$(document)
