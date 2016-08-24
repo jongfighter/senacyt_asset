e7e072655f70c5d6b28c4ca26a0f0127e43faa84
Sara KIM <sara940569@gmail.com>
2016-08-25 05:29:43 +0900

    insert css file & modify margin left

--- loc/form_list.php
+++ loc/form_list.php
@@ -111,3 +111,4 @@ echo "</table>";
   <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
   <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
 </html>
+ 
